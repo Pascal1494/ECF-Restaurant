a95020e6065ee980d4651ec6ca43bd940d0be08f
pascal1494 <discokilly@laposte.net>
2023-04-25 10:36:05 +0200

    Créate Openday's entity

---
+++ src/Entity/OpenDay.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OpenDayRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OpenDayRepository::class)]
+class OpenDay
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $day = null;
+
+    #[ORM\Column]
+    private ?bool $isMorningOpen = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $morningOpen = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $morningClose = null;
+
+    #[ORM\Column]
+    private ?bool $isEveningOpen = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $eveningOpen = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $eveningClose = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+   
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?string
+    {
+        return $this->day;
+    }
+
+    public function setDay(string $day): self
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function isIsMorningOpen(): ?bool
+    {
+        return $this->isMorningOpen;
+    }
+
+    public function setIsMorningOpen(bool $isMorningOpen): self
+    {
+        $this->isMorningOpen = $isMorningOpen;
+
+        return $this;
+    }
+
+    public function getMorningOpen(): ?\DateTimeInterface
+    {
+        return $this->morningOpen;
+    }
+
+    public function setMorningOpen(\DateTimeInterface $morningOpen): self
+    {
+        $this->morningOpen = $morningOpen;
+
+        return $this;
+    }
+
+    public function getMorningClose(): ?\DateTimeInterface
+    {
+        return $this->morningClose;
+    }
+
+    public function setMorningClose(\DateTimeInterface $morningClose): self
+    {
+        $this->morningClose = $morningClose;
+
+        return $this;
+    }
+
+    public function isIsEveningOpen(): ?bool
+    {
+        return $this->isEveningOpen;
+    }
+
+    public function setIsEveningOpen(bool $isEveningOpen): self
+    {
+        $this->isEveningOpen = $isEveningOpen;
+
+        return $this;
+    }
+
+    public function getEveningOpen(): ?\DateTimeInterface
+    {
+        return $this->eveningOpen;
+    }
+
+    public function setEveningOpen(\DateTimeInterface $eveningOpen): self
+    {
+        $this->eveningOpen = $eveningOpen;
+
+        return $this;
+    }
+
+    public function getEveningClose(): ?\DateTimeInterface
+    {
+        return $this->eveningClose;
+    }
+
+    public function setEveningClose(\DateTimeInterface $eveningClose): self
+    {
+        $this->eveningClose = $eveningClose;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    
+}
---
+++ src/Repository/OpenDayRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\OpenDay;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<OpenDay>
+ *
+ * @method OpenDay|null find($id, $lockMode = null, $lockVersion = null)
+ * @method OpenDay|null findOneBy(array $criteria, array $orderBy = null)
+ * @method OpenDay[]    findAll()
+ * @method OpenDay[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class OpenDayRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, OpenDay::class);
+    }
+
+    public function save(OpenDay $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(OpenDay $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return OpenDay[] Returns an array of OpenDay objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('o.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?OpenDay
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
