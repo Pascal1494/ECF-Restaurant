eb41341a147e3c0b15560b7874e571511185d81d
pascal1494 <discokilly@laposte.net>
2023-04-28 11:21:05 +0200

    modif  User's key

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -211,6 +211,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __toString()
     {
-        return $this->pseudo;
+        return $this->slug;
     }
 }
