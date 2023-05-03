7538c4e8e57ab229a6d167c039f88d7e47f3332a
pascal1494 <discokilly@laposte.net>
2023-05-03 07:42:42 +0200

    MaJ CSS footer

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -13,8 +13,38 @@ class Reservation
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $nbGuests = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNbGuests(): ?int
+    {
+        return $this->nbGuests;
+    }
+
+    public function setNbGuests(int $nbGuests): self
+    {
+        $this->nbGuests = $nbGuests;
+
+        return $this;
     }
 }
