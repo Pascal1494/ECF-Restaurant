f153a7bf0398688ac7fdc954644e1ab7265e5c38
pascal1494 <discokilly@laposte.net>
2023-05-04 16:56:23 +0200

    Add Nordics dishhes and CSS in home page

--- src/Controller/DisheController.php
+++ src/Controller/DisheController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\DisheRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DisheController extends AbstractController
 {
-    #[Route('/dishe', name: 'app_dishe')]
-    public function index(): Response
+    #[Route('/dishe/{slug}', name: 'app_dishe')]
+    public function index(string $slug, DisheRepository $disheRepository): Response
     {
+        $dishe = $disheRepository->findOneBy(['slug' => $slug]);
+
         return $this->render('dishe/index.html.twig', [
-            'controller_name' => 'DisheController',
+            'dishe' => $dishe
         ]);
     }
 }
