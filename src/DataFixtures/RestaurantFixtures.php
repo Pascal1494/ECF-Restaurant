81b88191fe62b45a19bdb53a7e1e4e9091ce8e84
Pascal1494 <discokilly@laposte.net>
2023-05-11 18:56:03 +0200

    Install  Datafixtures &  add restaurant fixtures

---
+++ src/DataFixtures/RestaurantFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Restaurant;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class RestaurantFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        for ($i = 0; $i < 1; $i++) {
+          $resto = new Restaurant();
+          $resto->setName('Le quai anthique');
+          $resto->setAddress('17 rue du quai');
+          $resto->setZipCode('63000');
+          $resto->setCountry('Chambery');
+          $resto->setphone('03 05 06 25 32');
+          $resto->setEmail('lqa-resto@gmail.com');
+          $resto->setMaxCapacity('50');
+          $manager->persist($resto);
+       }
+
+        $manager->flush();
+    }
+}
